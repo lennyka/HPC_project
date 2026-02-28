9ce56207c456bd3fe92aa3d75e1270dd7a1e374f
lennyka <lennyka59@gmail.com>
2026-02-28 16:41:58 +0100

    strong scaling sbatch

---
+++ scripts/orfeo/strong_scaling/strong_mem_3N.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=3                   # 1 node
+#SBATCH --ntasks=24                  # total MPI tasks across nodes
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=16          # OpenMP threads per MPI task
+#SBATCH --mem=0                     # use all available memory
+#SBATCH --partition=EPYC
+#SBATCH -t 00:30:00                 # 5 minutes for profiling and test runs
+#SBATCH --job-name=Strong_scaling
+
+# Set OpenMP variables
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+export OMP_DISPLAY_AFFINITY=TRUE
+
+# Load MPI module if needed
+module load openMPI/5.0.5
+
+mpicc -D_XOPEN_SOURCE=700 -o main -march=native -O3 -std=c17 -fopenmp -Iinclude ./src/stencil_parallel_mem_vec.c
+srun --ntasks=24 --cpus-per-task=16 --cpu-bind=cores  ./main -x 15000 -y 15000 -p 0 -o 0 -e 300 -v 1 > ./output/orfeo/strong_scaling/mem/output_strong_3_node_8taskpernode_16cpupertask.log
---
+++ scripts/orfeo/strong_scaling/strong_scaling_orfeo_2N.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=2                   # 1 node
+#SBATCH --ntasks=16                  # total MPI tasks across nodes
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=16          # OpenMP threads per MPI task
+#SBATCH --mem=0                     # use all available memory
+#SBATCH --partition=EPYC
+#SBATCH -t 00:30:00                 # 5 minutes for profiling and test runs
+#SBATCH --job-name=Strong_scaling
+
+# Set OpenMP variables
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+export OMP_DISPLAY_AFFINITY=TRUE
+
+# Load MPI module if needed
+module load openMPI/5.0.5
+
+mpicc -D_XOPEN_SOURCE=700 -o main -march=native -O3 -std=c17 -fopenmp -Iinclude ./src/stencil_template_parallel.c
+srun --ntasks=16 --cpus-per-task=16 --cpu-bind=cores ./main -x 15000 -y 15000 -p 0 -o 0 -e 300 -v 1 > ./output/orfeo/strong_scaling/baseline/output_strong_2_node_8taskpernode_16cpupertask.log
